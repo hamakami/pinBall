47fca44ea94a4459e0230ef1843dde9cc899b45f
kamihama <sshangbin@gmail.com>
2019-08-29 01:14:08 +0900

    pinBallプロジェクト新規作成
    課題のみ作成、動作確認を行いました。

---
+++ Assets/GetPointShow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetPointShow : MonoBehaviour
+{
+	//スコアを表示するテキスト
+	private GameObject GetPointText;
+	private int score = 0; //スコア計算用変数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//シーン中のGetPointTextオブジェクトを取得
+		this.GetPointText = GameObject.Find("GetPointText");
+		score   = 0;
+		SetScore();   //初期スコアを代入して表示
+    }
+
+    //小さい星と雲に衝突で＋100、大きい星と雲に衝突で＋200
+    void OnCollisionEnter(Collision collision)
+    {
+        string Tag = collision.gameObject.tag;
+
+		//Debug.Log (collision.gameObject.name);
+
+		if ((Tag == "LargeStarTag")||(Tag=="LargeCloudTag"))
+        {
+			Debug.Log (collision.gameObject.name);
+            score += 200;
+			//Destroy(collision.gameObject);
+        }
+		else if(Tag == "SmallCloudTag")
+        {
+			Debug.Log (collision.gameObject.name);
+            score += 100;
+			//Destroy(collision.gameObject);
+        }
+		/*else if(Tag == "SmallCloudTag")
+		{
+			score += 100;
+            //Destroy(gameObject);
+        }
+        else if (Tag == "SmallCloudPrefabTag")
+        {
+            score += 100;
+            //Destroy(gameObject);
+        }
+        else if (Tag == "LargeCloudTag")
+        {
+            score += 200;
+            //Destroy(gameObject);
+        }*/
+        SetScore();
+        
+    }
+
+    void SetScore()
+	{
+		//GetPointTextにスコアを表示
+		this.GetPointText.GetComponent<Text>().text = "Score:"+score;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tap.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tap : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        //SetAngle(this.defaultAngle);
+        
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            var toucth = Input.GetTouch(0);
+            if (toucth.phase == TouchPhase.Began)
+            {
+                //タッチ開始
+                Debug.Log("押した瞬間");
+
+            }
+
+            else if (toucth.phase == TouchPhase.Ended)
+            {
+                Debug.Log("離した瞬間");
+                  
+        }
+
+            if (toucth.phase == TouchPhase.Moved)
+            {
+                Debug.Log("押しっぱなし");
+            }
+        }
+      
+
+    }
+}
